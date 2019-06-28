e34c878662dffe677dea59bec6078b7b258ddfca
mkguytone <m.komatsu.05@cantab.net>
2019-06-29 00:41:29 +0900

    added head.js

---
+++ script/head.js
@@ -0,0 +1,17 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143005445-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-143005445-1');
+</script>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-6982397509579717",
+    enable_page_level_ads: true
+  });
+</script>
