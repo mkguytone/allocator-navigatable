00ee2d6a997c394437e42fd0d7449efe841c27b6
Masaki Komatsu <52219085+mkguytone@users.noreply.github.com>
2019-07-01 01:33:50 +0900

    Update head.js

--- script/head.js
+++ script/head.js
@@ -1,17 +1,15 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143005445-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143005445-1"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-143005445-1');
 
-  gtag('config', 'UA-143005445-1');
-</script>
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-6982397509579717",
-    enable_page_level_ads: true
-  });
-</script>
+document.write('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+(adsbygoogle = window.adsbygoogle || []).push({
+  google_ad_client: "ca-pub-6982397509579717",
+  enable_page_level_ads: true
+});
+
